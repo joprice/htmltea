78b49553d418e8975648350dc96cdcdba55f5f3d
Joseph Price <pricejosephd@gmail.com>
2023-08-05 08:21:01 -0400

    adapt to dream-html

--- bin/main.ml
+++ bin/main.ml
@@ -7,16 +7,53 @@ let read_stdin () =
   in
   loop ""
 
-let translate_attr = function
-  | "viewbox" -> "viewBox"
-  | "form" -> "Tea.Html2.Attributes.form"
-  | ("class" | "for" | "type" | "method") as attr -> attr ^ "'"
-  | other -> other
+let quoted value = "\"" ^ value ^ "\""
+
+module S = Set.Make (String)
+
+(* TODO: handle svg attrs *)
+let unhandled_attr =
+  S.of_list
+    [
+      "viewbox";
+      "stroke-width";
+      "stroke";
+      "fill";
+      "stroke-linecap";
+      "stroke-linejoin";
+      "d";
+    ]
+
+let is_unhandled_attr attr = unhandled_attr |> S.mem attr
+let is_numeric_attr attr = attr = "tabindex"
+
+let translate_attr key value =
+  let key, maybe_value =
+    match key with
+    | ("class" | "for" | "type" | "method") as attr -> (attr ^ "_", None)
+    | attr
+      when attr |> String.starts_with ~prefix:"aria-" || is_unhandled_attr attr
+      ->
+        if value = "true" || value = "false" then
+          (Format.sprintf "bool_attr \"%s\"" attr, Some value)
+        else (Format.sprintf "string_attr \"%s\"" attr, None)
+    | other -> (other, None)
+  in
+  let value =
+    match maybe_value with
+    | Some value -> value
+    | None ->
+        if is_numeric_attr key then
+          (* adding parens to handle negative numbers and letting ocamlforamt handle cleaning it up *)
+          Format.sprintf "(%s)" value
+        else quoted value
+  in
+  Format.sprintf "%s %s" key value
 
 let translate_element = function
-  | "form" -> "Tea.Html2.form"
-  | "path" -> "Tea.Svg.path"
-  | ("input" | "option" | "object" | "var") as element -> element ^ "'"
+  | "span" -> "Tag.span"
+  | ("input" | "option" | "object" | "var") as element -> element ^ "-"
+  | ("svg" | "path") as tag -> Format.sprintf "std_tag %s" (quoted tag)
   | other -> other
 
 let spaces i = String.make i ' '
@@ -26,12 +63,13 @@ let build_attrs e indent_width =
     e
     |> Soup.fold_attributes
          (fun acc key value ->
-           (spaces indent_width ^ translate_attr key ^ " \"" ^ value ^ "\"")
-           :: acc)
+           (spaces indent_width ^ translate_attr key value) :: acc)
          []
     |> List.rev
   in
   String.concat ";\n" attrs
+
+let is_void_tag tag = tag = "img"
 
 let rec convert p (state, depth) =
   p |> Soup.children
@@ -45,27 +83,29 @@ let rec convert p (state, depth) =
                List.rev (convert (Soup.coerce e) ([], depth + 1))
              in
              let children =
-               if List.length children == 0 then "[]"
+               if List.length children == 0 then
+                 if is_void_tag (Soup.name e) then "" else "[]"
                else
                  "[\n"
                  ^ String.concat "\n" (List.map (fun x -> x ^ ";") children)
                  ^ "\n" ^ indent ^ "]"
              in
              let name = translate_element (Soup.name e) in
-             let open_svg, close_svg =
-               if name = "svg" then ("Tea.Svg.(Tea.Svg.Attributes.(", "))")
-               else ("", "")
-             in
+             (*let open_svg, close_svg =
+                 if name = "svg" then ("Tea.Svg.(Tea.Svg.Attributes.(", "))")
+                 else ("", "")
+               in
+             *)
              let attrs = build_attrs e (indent_width + 2) in
              let output =
-               indent ^ open_svg ^ name ^ " [\n " ^ attrs ^ "\n" ^ indent
-               ^ " ] " ^ children ^ close_svg
+               indent ^ name ^ " [\n " ^ attrs ^ "\n" ^ indent ^ " ] "
+               ^ children
              in
              output :: state
          | None ->
              let text = String.concat " " (Soup.trimmed_texts e) in
              if String.length text = 0 then state
-             else (indent ^ "text \"" ^ text ^ "\"") :: state)
+             else (indent ^ "txt \"" ^ text ^ "\"") :: state)
        state
 
 let read_url url =
@@ -81,9 +121,19 @@ let read_url url =
       Error e
 
 let parse html =
+  print_endline
+    "let element = let open Dream_html in let open Tag in let open Attr in ";
   let parsed = Soup.parse html in
   convert (Soup.coerce parsed) ([], 0)
 
 let () =
-  let arg = Sys.argv.(1) in
-  Result.map parse (read_url arg) |> Result.iter (List.iter print_endline)
+  let contents =
+    if Sys.argv |> Array.length = 2 then
+      let arg = Sys.argv.(1) in
+      read_url arg
+    else Ok (read_stdin ())
+  in
+  Result.map parse contents
+  |> Result.fold ~ok:(List.iter print_endline) ~error:(fun e ->
+         Format.printf "Failed: %a" Curly.Error.pp e;
+         exit 1)
